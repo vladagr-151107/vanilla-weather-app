2078c0db4c7f82ca733061a8f94377ffdfe1972b
vladagr-151107 <vladagrushchenko@gmail.com>
2022-11-18 17:44:43 +0200

    added function with current time and weather

--- src/java.js
+++ src/java.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -7,8 +21,13 @@ function showTemperature(response) {
     descriptionElement.innerHTML = response.data.condition.description;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.time*1000);
 }
+let city = "Odesa";
 let apiKey = "e43d0522c6a2b491f8bte6b227o4172b";
 let apiUrl =
-    `https://api.shecodes.io/weather/v1/current?query=Odesa&key=${apiKey}&units=metric`;
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
