c81bce9b7fa4e1c40983cf55e8b50f17adc83f65
vladagr-151107 <vladagrushchenko@gmail.com>
2022-11-19 20:16:26 +0200

    added conversion

--- src/java.js
+++ src/java.js
@@ -12,6 +12,11 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function search(city) {
+  let apiKey = "e43d0522c6a2b491f8bte6b227o4172b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -31,13 +36,7 @@ function showTemperature(response) {
         `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     iconElement.setAttribute("alt", response.data.condition.description);
 }
-celsiusTemperature = Math.round(response.data.temperature.current);
-function search(city) {
-    let apiKey = "e43d0522c6a2b491f8bte6b227o4172b";
-    let apiUrl =
-        `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showTemperature);
-}
+celsiusTemperature = response.data.temperature.current;
 search("Odesa");
 function handleSubmit(event) {
     event.preventDefault();
@@ -49,11 +48,21 @@ form.addEventListener("submit", handleSubmit);
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
-    let fahrenheitTemperature = (temperatureElement.innerHTML * 9) / 5 + 32;
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
 }
 let celsiusTemperature = null;
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+search("Odesa");
