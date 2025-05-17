ee5c4a465dcef28c6b1fd69520a7507c29efb58e
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-05-17 15:16:55 +0300

    added saving session ability

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from models import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
 db.init_app(app)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -39,10 +38,18 @@ def login():
         if not user or not check_password_hash(user.password, password):
             return render_template("login.html", error="Wrong username or password")
 
+        session["username"] = user.username
         return redirect(url_for("home"))
 
     return render_template("login.html")
-
+@app.route("/")
+def home():
+    username = session.get("username")
+    return render_template("index.html", username=username)
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("home"))
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
